e26d1b3945e859b960069abdfdbd3fca39e64efb
Melvin <melvin.courant@lasuiteandco.com>
2026-05-18 17:40:21 +0200

    :technologist: add travel seeder

---
+++ database/seeders/02_travel_seeder.ts
@@ -0,0 +1,16 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Travel from '#models/travel'
+import User from "#models/user";
+
+export default class extends BaseSeeder {
+  async run() {
+    const user = await User.findByOrFail('email', 'test@test.fr')
+
+    await Travel.createMany([
+      {
+        title: 'Amazonie',
+        userId: user.id
+      }
+    ])
+  }
+}
